d96493991806e19795155ad89516eb74c4ee3a28
Jim Raynor <paolo@combi.li>
2019-07-13 21:56:50 +0300

    update new version

--- dev/scripts/host.sh
+++ dev/scripts/host.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
+export COMPOSE_PROJECT_NAME=mementodockeruse
 
-COMPOSE_PROJECT_NAME=project
 
-SLIM_HOST=project.local
-SLIM_HOST_ALIASES=""
+NETWORK=${COMPOSE_PROJECT_NAME}_project
+
+HOST=project.local
+
+
+apache_cntr=`docker-compose ps -q apache`
+apache_ip=`docker inspect -f "{{.NetworkSettings.Networks.${NETWORK}.IPAddress}}" $apache_cntr`
+
+
+phpmyadmin_cntr=`docker-compose ps -q phpmyadmin`
+phpmyadmin_ip=`docker inspect -f "{{.NetworkSettings.Networks.${NETWORK}.IPAddress}}" $phpmyadmin_cntr`
+
+
+adminer_cntr=`docker-compose ps -q adminer`
+adminer_ip=`docker inspect -f "{{.NetworkSettings.Networks.${NETWORK}.IPAddress}}" $adminer_cntr`
+
+
+mariadb_cntr=`docker-compose ps -q mariadb`
+mariadb_ip=`docker inspect -f "{{.NetworkSettings.Networks.${NETWORK}.IPAddress}}" $mariadb_cntr`
+
+rabbitmq_cntr=`docker-compose ps -q rabbitmq`
+rabbitmq_ip=`docker inspect -f "{{.NetworkSettings.Networks.${NETWORK}.IPAddress}}" $rabbitmq_cntr`
+
 
 
 # Add hosts mapping for Magento in host machine
 sudo -p "[sudo] Need admin privileges to write to /etc/hosts: " -s -- <<EOF
-    # Remove existing host mappings
-sed -i "/${SLIM_HOST}/d" /etc/hosts
+# Remove existing host mappings
+sed -i "/${HOST}/d" /etc/hosts
 
 
-# Export container IPs
-echo "`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' doodleonchain_apache_1` ${SLIM_HOST} ${SLIM_HOST_ALIASES}" >> /etc/hosts
-echo "`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' doodleonchain_adminer_1` adminer.${SLIM_HOST}" >> /etc/hosts
-echo "`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' doodleonchain_phpmyadmin_1` phpmyadmin.${SLIM_HOST}" >> /etc/hosts
-echo "`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' doodleonchain_rabbitmq_1` rabbitmq.${SLIM_HOST}" >> /etc/hosts
-
+echo "${apache_ip} ${HOST} " >> /etc/hosts
+echo "${phpmyadmin_ip} phpmyadmin.${HOST}" >> /etc/hosts
+echo "${adminer_ip} adminer.${HOST}" >> /etc/hosts
+echo "${rabbitmq_ip} rabbitmq.${HOST}" >> /etc/hosts
 
 
 EOF
-
